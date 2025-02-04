e0bbcd209a0028d53674d9d5a49aee1fb40bdc80
Jenny <jenny@circleci.com>
2025-02-04 12:45:49 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# circleci-usage-api-exporter
+# CircleCI Usage API Exporter
+
+---
+**Disclaimer:**
+
+CircleCI Labs, including this repo, is a collection of solutions developed by members of CircleCI's field engineering teams through our engagement with various customer needs.
+
+-   ✅ Created by engineers @ CircleCI
+-   ✅ Used by real CircleCI customers
+-   ❌ **not** officially supported by CircleCI support
+
+---
+
+## Introduction
+
+This tool outlines using the CircleCI Usage API to create and download usage reports. The data is then merged and transformed into a graph to show credit usage per project.
+
+For more info on the API itself, visit the docs [here](https://circleci.com/docs/api/v2/index.html#tag/Usage).
+
+All the outputs are saved as an [artifact](https://circleci.com/docs/artifacts/) on CircleCI.
+
+### Use Cases
+
+While the implementation shown in this project is simple, there are many use cases for implementing the Usage API in this way. 
+
+Some of the advantages include:
+
+- [Scheduling the pipeline](https://circleci.com/docs/scheduled-pipelines/) to run weekly, to enable users to target projects that have a higher credit usage
+- Enabling the comparison of weekly results
+- Can be combined with the [Slack orb](https://circleci.com/developer/orbs/orb/circleci/slack) to send notifications on specific usage metrics
+- Can be amended to target job-level data instead, to track the cost of failing jobs
+- Can group projects by team, to enable cross-company billing
+
+## Tools
+
+To learn more about working with `*.csv` files, and transforming the data once it's downloaded, check out [pandas](https://pandas.pydata.org/).
+
+To learn more about graphs using python, check out [Matplotlib](https://matplotlib.org/stable/).
+
+## Requirements
+
+- A CircleCI [personal API token](https://circleci.com/docs/managing-api-tokens/#creating-a-personal-api-token) is required in order to use the API. This is saved with the name `CIRCLECI_API_TOKEN`, in a context.
+- A date range is required. These are specified using the `START_DATE` and `END_DATE` environment variables
+- An organisation ID is required. This defaults to the ID of the organisation that is executing the job on CircleCI.
+
+### Caveats
+
+- My python skillz aren't great
---
+++ src/get_usage_report.py
@@ -0,0 +1,80 @@
+# Import modules
+import os
+import requests
+import json
+import time
+import gzip
+import sys
+
+# Build data to send with requests
+ORG_ID = os.getenv('ORG_ID')
+CIRCLECI_TOKEN = os.getenv('CIRCLECI_API_TOKEN')
+START_DATE = os.getenv('START_DATE')
+END_DATE = os.getenv('END_DATE')
+
+post_data = {
+    "start": f"{START_DATE}T00:00:01Z",
+    "end": f"{END_DATE}T00:00:01Z",
+    "shared_org_ids": []
+}
+
+# Request the usage report
+response = requests.post(
+    f"https://circleci.com/api/v2/organizations/{ORG_ID}/usage_export_job",
+    headers={"Circle-Token": CIRCLECI_TOKEN, "Content-Type": "application/json"},
+    data=json.dumps(post_data)
+)
+
+# Once requested, the report can take some time to process, so a retry is built-in
+if response.status_code == 201:
+    print("Report requested successfully")
+    data = response.json()
+    USAGE_REPORT_ID = data.get("usage_export_job_id")
+    print(f"Report ID is {USAGE_REPORT_ID}")
+    
+    # Check if the report is ready for downloading as it can take a while to process
+    for i in range(5):
+        print("Checking if report can be downloaded")
+        report = requests.get(
+            f"https://circleci.com/api/v2/organizations/{ORG_ID}/usage_export_job/{USAGE_REPORT_ID}",
+            headers={"Circle-Token": CIRCLECI_TOKEN}
+        ).json()
+
+        report_status = report.get("state")
+
+        # Download the report and save it
+        if report_status == "completed":
+            print("Report generated. Now Downloading...")
+            download_urls = report.get("download_urls", [])
+
+            if not os.path.exists("reports"):
+                os.makedirs("/tmp/reports")
+            
+            for idx, url in enumerate(download_urls):
+                r = requests.get(url)
+                with open(f"/tmp/usage_report_{idx}.csv.gz", "wb") as f:
+                    f.write(r.content)
+                
+                with gzip.open(f"/tmp/usage_report_{idx}.csv.gz", "rb") as f_in:
+                    with open(f"/tmp/reports/usage_report_{idx}.csv", "wb") as f_out:
+                        f_out.write(f_in.read())
+
+                print(f"File {idx} downloaded and extracted")
+
+            print("All files downloaded and extracted to the /reports directory")
+            break
+        
+        elif report_status == "processing":
+            print("Report still processing. Retrying in 1 minute...")
+            time.sleep(60)  # Wait for 60 seconds before retrying
+        
+        else:
+            print(f"Report status: {report_status}. Error occurred.")
+            break
+    else:
+        print("Report is still in processing state after 5 retries.")
+        sys.exit(1)
+else:
+    # Exit if something else happens, like requests are being throttled
+    print(f"{response}")
+    sys.exit(1)
